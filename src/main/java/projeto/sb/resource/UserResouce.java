12f5a338fc8a7927874b59e1289d19e06bc92225
Gabriel Andrade <andradeoliveirabiel@gmail.com>
2026-03-31 15:51:49 -0300

    primeiras aplicacoes de usuario

---
+++ src/main/java/projeto/sb/resource/UserResouce.java
@@ -0,0 +1,20 @@
+package projeto.sb.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import projeto.sb.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResouce {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u1 = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u1);
+	}
+	
+}
