cf2635d1eb3cccbaa95bdbb341f15f394987fee8
Gabriel Andrade <andradeoliveirabiel@gmail.com>
2026-04-07 10:55:14 -0300

    order page created

--- src/main/java/projeto/sb/resource/UserResource.java
+++ src/main/java/projeto/sb/resource/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,12 @@ public class UserResource {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long Id) {
+		User obj = service.findById(Id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 }
