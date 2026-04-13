5c85f344625d526e7dc41a80f65ecb97c291e468
Gabriel Andrade <andradeoliveirabiel@gmail.com>
2026-04-13 10:33:45 -0300

    CategoryService, CategoryRepository and CategoryResource created

---
+++ src/main/java/projeto/sb/service/CategoryService.java
@@ -0,0 +1,26 @@
+package projeto.sb.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import projeto.sb.entities.Category;
+import projeto.sb.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long Id) {
+		Optional<Category> obj = repository.findById(Id); 
+		return obj.orElseThrow(()-> new RuntimeException("usuario nao encontrado."));
+	}
+}
